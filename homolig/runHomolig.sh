c28dd40e77593ae7c826363fed4e82824695a64e
Alexander Girgis <agirgis3@jhmi.edu>
2023-01-02 14:30:42 -0500

    added wrapper

--- homolig/runHomolig.sh
+++
@@ -1,7 +0,0 @@
-input_file=F:/homolig/homolig/data/test_data/bcr-paired-test.csv
-seq_type=bcr
-chains=paired
-metric=aadist
-species=human
-
-python runHomolig.py -i $input_file -s $seq_type -c $chains -m $metric -p $species
