ab3629da3b5f45d500d1edfeab362c3c51f7b57d
Alexander Girgis <91854339+agirgis3@users.noreply.github.com>
2024-10-15 10:57:31 -0400

    Update python-testcode.sh

--- python-testcode.sh
+++ python-testcode.sh
@@ -1,6 +1,25 @@
+#homolig_format_checker.py confirms format of the input file. It will report to you any uninterpretable V gene calls and the number of duplicated or otherwise invalid amino acid sequences. 
+
+#homolig_wrapper.py calculates similarity scores between sequences using the similarity matrix specified in --metric. 
+# pairwise mode will calculate all similarity scores among members resulting in nxn output matrix in .h5ad format. 
+# axb mode will cross two input files resulting in axb output matrix. See homolig_wrapper.py -h
+
+#homolig_write_umap.py clusters the resultant similarity matrix from homolig_wrapper.py. 
+# It will also report silhouette scores in output file.
+
+#homolig_write_umap.py generates UMAP coordinates based on the input similiarity matrix. 	
+
+#h5ad_to_csv.py converts .h5ad output format of homolig_wrapper.py into a .csv file for those interested in manipulating the raw data. 	
+
+#Three series of examples are provided in this code. 
+#(i) Paired chain data from anti-viral TCRs in VDJDb used in manuscript. 
+#(ii) Example TCRB data from one of the HC samples used in manuscript. 
+#(iii) Example IGH data from one of the HC samples used in manuscript.  
+
 ####Process paired-chain viral TCR data
 
-python3 ./homolig/homolig_format_checker.py\
+
+python3 ./homolig/homolig_format_checker.py\ 
 		-i ./test-data/TCR-paired-test_viral.csv\
 		--seq tcr \
 		--chains paired \
@@ -9,6 +28,7 @@ python3 ./homolig/homolig_format_checker.py\
 		--save_germline False\
 		--verbose True \
 		--output ./vdjdb_paired_ens.h5ad
+
 
 python3 ./homolig/homolig_wrapper.py \
 		-i ./test-data/TCR-paired-test_viral.csv\
@@ -20,11 +40,11 @@ python3 ./homolig/homolig_wrapper.py \
 		--verbose True \
 		--output ./vdjdb_paired_ens.h5ad
 		
-		
+ 		
 python3 ./homolig/clusterHomolig.v4.py \
 		-i ./vdjdb_paired_ens.h5ad \
 		--num_clusters 80
-		
+
 python3 ./homolig/homolig_write_umap.py \
 	-i ./vdjdb_paired_ens.h5ad\
 	-o ./vdjdb_paired_ens_umap.csv	
